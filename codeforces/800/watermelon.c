ff288afad2e1a59839ef091349ad0fc9c88e0640
Sifat Islam <117737171+sifat6472@users.noreply.github.com>
2022-12-22 21:59:46 +0600

    Add files via upload

---
+++ codeforces/800/watermelon.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int melon;
+    scanf("%d",&melon);
+
+if(melon%2==0 && melon!=2){
+        printf("Yes\n");
+        }
+    else {
+        printf("No\n");
+        }
+    
+    return 0;
+
+}
